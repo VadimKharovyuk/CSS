03de05c1acfa7d98c646e52a0975621736bfe2e2
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-03-03 05:28:37 +0530

    color

--- src/main/java/com/example/css/CssApplication.java
+++ src/main/java/com/example/css/CssApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CssApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CssApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CssApplication.class, args);
+    }
 
 }
--- src/main/java/com/example/css/controller/CssController.java
+++ src/main/java/com/example/css/controller/CssController.java
@@ -1,2 +1,13 @@
-package com.example.css.controller;public class CssController {
+package com.example.css.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class CssController {
+    @GetMapping("/")
+    public String home(){
+        return "style_color";
+    }
 }
